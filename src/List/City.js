360f79083fe45dd94f8adb101e450788df55aebe
Mohammed <Mohammed.ofisan@hotmail.com>
2019-11-06 17:23:49 +0300

    refctor code

--- src/List/City.js
+++ src/List/City.js
@@ -8,9 +8,7 @@ export default class City extends Component {
         return (
             <div className="bar">
 
-                <h2><Button variant="outline-danger" onClick={() => this.props.deleteone(this.props.data)}>delete</Button>
-                 {this.props.data.country} - {this.props.data.cityName}   
-                 {this.props.data.weather.toFixed(0)}°C  {this.props.data.main}</h2>
+                <h2><Button variant="outline-danger" onClick={() => this.props.deleteone(this.props.data)}>delete</Button> {this.props.data.country} - {this.props.data.cityName}   {this.props.data.weather.toFixed(0)}°C  {this.props.data.main}</h2>
             </div>
         )
     }
