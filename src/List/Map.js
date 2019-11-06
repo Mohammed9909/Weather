add7768218230ec9652f57bd8487c862356ea2c5
Mohammed <Mohammed.ofisan@hotmail.com>
2019-11-06 16:11:51 +0300

    Deployment setup

--- src/List/Map.js
+++ src/List/Map.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Map extends Component {
-  constructor(){
+  constructor() {
     super()
-  
+
     axios.get(`https://maps.googleapis.com/maps/api/js?key=AIzaSyAyEMuBiLJrxjEZjw_-SV-BEirGTuCRBOg&libraries=geometry,places`)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
     return (
       <div>
-        
+
       </div>
     )
   }
