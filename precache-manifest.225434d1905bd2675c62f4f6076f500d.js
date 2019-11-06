8d3b0f710e1e5b70e590157e3b4cee69a1804be7
Mohammed <Mohammed.ofisan@hotmail.com>
2019-11-06 16:14:22 +0300

    Updates

---
+++ precache-manifest.225434d1905bd2675c62f4f6076f500d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecc6793a325fa7f145e5c7e4c24afc2",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "72c82c6506ddfc38fbe1",
+    "url": "/Weather/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8f7f2bf632786c7a9396",
+    "url": "/Weather/static/css/main.de77af13.chunk.css"
+  },
+  {
+    "revision": "72c82c6506ddfc38fbe1",
+    "url": "/Weather/static/js/2.470f8dfb.chunk.js"
+  },
+  {
+    "revision": "8f7f2bf632786c7a9396",
+    "url": "/Weather/static/js/main.18c69663.chunk.js"
+  },
+  {
+    "revision": "875ea904aca0256bb8cb",
+    "url": "/Weather/static/js/runtime-main.1e7d661c.js"
+  },
+  {
+    "revision": "ed374bc9a29338680fa0b7cd36d2d2f6",
+    "url": "/Weather/static/media/1.ed374bc9.gif"
+  }
+]);
